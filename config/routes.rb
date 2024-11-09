dd617119be3c9b1725e2a63cdea97609eed40499
Wadia Jara Quesieh <wjaraquesieh@academic.rrc.ca>
2024-11-08 18:39:42 -0600

    Create home controller with index view and quick menu bar

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get "users/show"
   get "categories/index"
   get "categories/show"
+  get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -25,4 +26,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "home#index"
 end
