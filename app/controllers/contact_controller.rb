f675a18cef8d0daf66c7815c15f0154f461ddc81
Wadia Jara Quesieh <wjaraquesieh@academic.rrc.ca>
2024-12-04 11:35:37 -0600

    Creation of Contact and about page, show a brief description an a contact form that is saved on db

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,18 @@
 class ContactController < ApplicationController
   def index
   end
+
+  def send_message
+    @contact_params = contact_params
+    ContactMailer.with(contact_params: @contact_params).contact_message.deliver_now
+
+    flash[:notice] = "Thank you for your message! We will get back to you soon."
+    redirect_to contact_path
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
 end
