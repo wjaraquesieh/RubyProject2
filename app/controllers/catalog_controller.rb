24faf6a7e57f08086685bb2869a3cf1584abd353
Wadia Jara Quesieh <wjaraquesieh@academic.rrc.ca>
2024-12-04 11:33:50 -0600

    Catalog information showing all products without filter yet

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -7,4 +7,13 @@ class CatalogController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    @query = params[:query]
+
+    @products = Product.where("name LIKE ?", "%#{@query}%")
+    @products = @products.page(params[:page]).per(15)
+
+    render :index
+  end
 end
